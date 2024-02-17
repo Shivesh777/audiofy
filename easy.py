31a0f64bd7221c79adb244bef4799259ba506aea
Bhayva Bhatt <76279956+Bhavya1435@users.noreply.github.com>
2024-02-17 16:28:26 -0500

    Update easy.py

--- easy.py
+++ easy.py
@@ -11,12 +11,12 @@ st.set_page_config(layout="wide", page_title="Image Background Remover")
 background_image = "background.jpg"  # Provide the path to your background image
 
 # Set page background
-page_bg_img = '''
+page_bg_img = f'''
 <style>
-body {
-background-image: url("https://example.com/background.jpg");
+body {{
+background-image: url("{background_image}");
 background-size: cover;
-}
+}}
 </style>
 '''
 
@@ -32,17 +32,25 @@ def convert_image_to_bytes(image):
     image.save(img_buffer, format="PNG")
     return img_buffer.getvalue()
 
+# Function to get download link for WAV file
+def get_wav_download_link(wav_file_path):
+    with open(wav_file_path, "rb") as file:
+        wav_file_data = file.read()
+    b64 = base64.b64encode(wav_file_data).decode()
+    href = f'<a href="data:audio/wav;base64,{b64}" download="audio.wav">Download WAV file</a>'
+    return href
+
 # Main function to run the app
 def main():
     # Buttons to switch between sections
     st.sidebar.write("")
     st.sidebar.write("")
-    section = st.sidebar.radio("", ('Upload Image', 'Download Image'), index=0)
+    section = st.sidebar.radio("", ('Upload Image', 'Download Audio'), index=0)
 
     if section == 'Upload Image':
         upload_image_section()
-    elif section == 'Download Image':
-        download_image_section()
+    elif section == 'Download Audio':
+        download_audio_section()
 
 # Section to upload image and remove background
 def upload_image_section():
@@ -61,23 +69,14 @@ def upload_image_section():
                 fixed_image = remove_background(image)
             st.image(fixed_image, caption="Background Removed", use_column_width=True)
 
-            # Download button
-            st.markdown(get_image_download_link(fixed_image), unsafe_allow_html=True)
+# Section to download audio
+def download_audio_section():
+    st.write("## Download WAV File")
+    st.write("Click the button below to download your WAV file.")
 
-# Section to download fixed image
-def download_image_section():
-    st.write("## Download Fixed Image")
-    st.write(":arrow_down: Click the button below to download the fixed image.")
-
+    wav_file_path = "dog.wav"
     # Download button
-    st.markdown(get_image_download_link(fixed_image), unsafe_allow_html=True)
-
-# Function to get download link for image
-def get_image_download_link(image):
-    buffered = convert_image_to_bytes(image)
-    b64 = base64.b64encode(buffered).decode()
-    href = f'<a href="data:file/png;base64,{b64}" download="fixed_image.png">Download fixed image</a>'
-    return href
+    st.markdown(get_wav_download_link(wav_file_path), unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
