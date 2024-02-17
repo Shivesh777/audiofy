3aa09f962a296b4c338ed8575422dc37bdbda3c1
Rishit Dagli <rishit.dagli@gmail.com>
2024-02-17 20:44:27 +0000

    update depth scales

--- models/main.py
+++ models/main.py
@@ -54,7 +54,56 @@ def load_segmentation_model():
     return sam
 
 
-def infer(image_path):
+def stitch(audio_files, coordinates, room_dimensions, output_path):
+    """
+    Create spatial audio by simulating sound propagation in a virtual room.
+
+    Parameters:
+    - audio_files (np.array): Array of audio data.
+    - coordinates (np.array): Array of 3D coordinates corresponding to the positions of audio data.
+    - room_dimensions (list): List containing the dimensions of the virtual room in meters [length, width, height].
+    - output_path (str): File path where the spatial audio will be saved.
+
+    Returns:
+    None
+
+    Note:
+    - The function assumes all audio files have the same sampling rate.
+    - The listener's position (receiver) is set at the center of the room's ground level.
+    - The simulated spatial audio is saved to the specified output path.
+
+    """
+    # Create an empty room
+    room = pra.ShoeBox(room_dimensions)
+    print(coordinates)
+
+    # Place sources in the room
+    for i, coord in enumerate(coordinates):
+        audio = audio_files[i][0][0]
+        print(audio)
+
+        my_source = pra.SoundSource(coord, signal=audio)
+        room.add_source(my_source)
+
+    # Set receiver position (listener's position)
+    receiver_pos = [room_dimensions[0] / 2, room_dimensions[1] / 2, 0]
+    room.add_microphone(receiver_pos)
+
+    # Compute RIRs
+    room.compute_rir()
+
+    # Simulate sound propagation in the room
+    room.simulate()
+
+    # Retrieve the simulated spatial audio signal received by the microphone
+    print(room.mic_array.signals)
+    output_signal = room.mic_array.signals[0]
+
+    # Save the spatial audio to a file
+    wavfile.write(output_path, 16000, output_signal)
+
+
+def infer(image_path, output_path):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     transform = Compose(
@@ -123,52 +172,12 @@ def infer(image_path):
             ddim_steps=500,
         )[0]
         audio.append(audio_wave)
-        
+    
+    depth  = ((-1 * (depth - 255.0))*(.1*w/255.)).detach().cpu().numpy()
+    points = torch.floor(torch.Tensor(points)).to(torch.int32).detach().cpu().numpy()
+    depth_values = depth[points[:, 1], points[:, 0]]
+    points = np.hstack((points, depth_values.reshape(-1, 1)))
+    
+    stitch(audio, points, [w, h , 3*w], output_path)
 
-def stitch(audio_files, coordinates, room_dimensions, output_path):
-    """
-    Create spatial audio by simulating sound propagation in a virtual room.
-
-    Parameters:
-    - audio_files (np.array): Array of audio data.
-    - coordinates (np.array): Array of 3D coordinates corresponding to the positions of audio data.
-    - room_dimensions (list): List containing the dimensions of the virtual room in meters [length, width, height].
-    - output_path (str): File path where the spatial audio will be saved.
-
-    Returns:
-    None
-
-    Note:
-    - The function assumes all audio files have the same sampling rate.
-    - The listener's position (receiver) is set at the center of the room's ground level.
-    - The simulated spatial audio is saved to the specified output path.
-
-    """
-    # Room parameters
-    fs, _ = wavfile.read(audio_files[0])  # Assuming all audio files have the same sampling rate
-
-    # Create an empty room
-    room = pra.ShoeBox(room_dimensions)
-
-    # Place sources in the room
-    for i, coord in enumerate(coordinates):
-        audio = audio_files[i]
-
-        my_source = pra.SoundSource(coord, signal=audio)
-        room.add_source(my_source)
-
-    # Set receiver position (listener's position)
-    receiver_pos = [room_dimensions[0] / 2, room_dimensions[1] / 2, 0]
-    room.add_microphone(receiver_pos)
-
-    # Compute RIRs
-    room.compute_rir()
-
-    # Simulate sound propagation in the room
-    room.simulate()
-
-    # Retrieve the simulated spatial audio signal received by the microphone
-    output_signal = room.mic_array.signals[0]
-
-    # Save the spatial audio to a file
-    wavfile.write(output_path, fs, output_signal)
+infer("images/tiger.jpg", "output.wav")
